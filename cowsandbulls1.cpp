0834e5104b021cd319b4c506282a0ddac9f88d3f
imaksymovv <Ivan.Vasiliy.Maksymov@gmail.com>
2022-07-26 17:15:53 +0200

    cows and bulls game modified with classes
    fixed problems with functions cowscounter and bulls counter
    added function your_number
    changed class name to cowsandbulls
    added descriptions to the functions

--- cowsandbulls1.cpp
+++ cowsandbulls1.cpp
@@ -1,92 +1,114 @@
 ﻿#include <iostream>
 
-int main() {
-	srand(time(NULL));
-	int a[4];
-	for (int i = 0; i < 4; ++i) {
-	a: a[i] = 1 + rand() % 9;
-		for (int j = 0; j < i; j++)
-			if (a[j] == a[i]) goto a; //сделано, что бы числа не повторялись
-	}
-	/*for (int i = 0; i < 4; ++i) {
-		std::cout << a[i]; // увидеть что загадал компьютер, для быстрого прохождения
-	} std::cout << "\n";*/
-	int b[4];
-	int c = 0;
-	int d = 0;
-	std::cout << "the computer guessed the number, try to guess it" << std::endl;
-	do {
+class cowsandbulls {
+public:
+	void cowscounter(int b[4], int a[4], int& c) { // считает и выводит кол-во коров
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (b[i] == a[j]) {
+					c = c + 1;
+				}
+			}
+		}
+	};
+	void bullscounter(int b[4], int a[4], int& c) { // считает и выводит кол-во быков
 		for (int i = 0; i < 4; ++i) {
 			std::cin >> b[i];
 			if (b[i] == a[i]) {
 				c = c + 1;
 			}
 		}
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				if (b[i] == a[j]) {
-					d = d + 1;
+	};
+	int computer_number(int a[4]) { // функция, благодаря которой компьютер загадывает число
+		for (int i = 0; i < 4; ++i) {
+			a[i] = 1 + rand() % 9;
+			for (int j = 0; j < i; j++)
+				if (a[j] == a[i]) {
+					do {
+						a[i] = 1 + rand() % 9;
+					} while (a[i] == a[j]);
 				}
-			}
 		}
+		return 0;
+	};
+	int your_number(int a[4]) { // функция, благодаря которой игрок загадывает число
+		for (int i = 0; i < 4; ++i) {
+			std::cin >> a[i];
+		}
+		return 0;
+	};
+};
+
+
+int main() {
+	srand(time(NULL));
+	int a[4];
+	cowsandbulls n1;
+	n1.computer_number(a);
+	for (int i = 0; i < 4; ++i) {
+		std::cout << a[i]; // позволяет увидеть что загадал компьютер, для быстрого прохождения(для обычного прохождения, можно удалить)
+	} std::cout << "\n";
+	int b[4];
+	int c = 0;
+	int d = 0;	
+	std::cout << "the computer guessed the number, try to guess it" << std::endl;
+	do {
+		cowsandbulls c1;
+		c1.bullscounter(b, a, c);
+		c1.cowscounter(b, a, d);
 		if (c != 4) {
 			std::cout << c << " bulls" << std::endl;
 			std::cout << d << " cows" << std::endl;
 			c = 0;
 			d = 0;
 		}
-	} while (c != 4);
+	} while (c != 4); // цикл, в котором игрок угадывает число компьютера
 	std::cout << "you did it! now it's computer turn to guess, enter your number" << std::endl;
 	int ur[4];
-	for (int i = 0; i < 4; ++i) {
-		std::cin >> ur[i];
-	}
+	n1.your_number(ur);
 	int c1 = 0;
 	int d1 = 0;
 	int a1[4];
 	int result[4];
 	int counter = 0;
+do {
+
+	do{
+		for (int i = 0; i < 4; ++i) {
+			a1[i] = rand() % 9;
+			counter++;
+		}
+	} while (a1[3] != ur[3]);
+	result[3] = a1[3];
+	c1 = 1;
+
 	do {
-		b: for (int i = 0; i < 4; ++i) {
+		for (int i = 0; i < 3; ++i) {
 			a1[i] = rand() % 9;
 			counter++;
 		}
-		if (a1[3] == ur[3]) {
-			result[3] = a1[3];
-			c1 = 1;
-		}
-		else goto b;
+	} while (a1[2] != ur[2]);
+	result[2] = a1[2];
+	c1 = 2;
 
-		c: for (int i = 0; i < 3; ++i) {
+	do {
+		for (int i = 0; i < 2; ++i) {
 			a1[i] = rand() % 9;
 			counter++;
 		}
-		if (a1[2] == ur[2]) {
-			result[2] = a1[2];
-			c1 = 2;
-		}
-		else goto c;
+	} while (a1[1] != ur[1]);
+	result[1] = a1[1];
+	c1 = 4;
 
-		d: for (int i = 0; i < 2; ++i) {
+	do {
+		for (int i = 0; i < 1; ++i) {
 			a1[i] = rand() % 9;
 			counter++;
 		}
-		if (a1[1] == ur[1]) {
-			result[1] = a1[1];
-			c1 = 3;
-		}
-		else goto d;
-
-		f: for (int i = 0; i < 1; ++i) {
-			a1[i] = rand() % 9;
-			counter++;
-		}
-		if (a1[0] == ur[0]) {
-			result[0] = a1[0];
-			c1 = 4;
-		}
-		else goto f;
-	} while (c1 != 4);
+	} while (a1[0] != ur[0]);
+	result[0] = a1[0];
+	c1 = 4;
+	} while (c1 != 4); // цикл, в котором компьютер угадывает число игрока
 	std::cout << "computer guessed ur number! it is:" << std::endl;
 	for (int i = 0; i < 4; ++i) {
 		std::cout << result[i];
